c5956e4f541afa8d09f7813248613b3f708625a7
Matan Botansky <65315115+botansky@users.noreply.github.com>
2022-03-05 11:41:19 -0500

    added BoardController

---
+++ Source/PaintersSummit/Private/BoardController.cpp
@@ -0,0 +1,44 @@
+// Author: Matan Botansky
+
+
+#include "BoardController.h"
+
+// Sets default values
+ABoardController::ABoardController()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void ABoardController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABoardController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MoveForward();
+}
+
+// Called to bind functionality to input
+void ABoardController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	InputComponent->BindAxis("Horizontal", this, &ABoardController::MoveHorizontal);
+}
+
+void ABoardController::MoveHorizontal(float HorizontalMovement)
+{
+	if (HorizontalMovement && HorizontalMovement != 0.0f) {
+		AddMovementInput(GetActorRightVector(), HorizontalMovement * HorizontalMovementBaseSpeed);
+	}
+}
+
+void ABoardController::MoveForward()
+{
+	AddMovementInput(GetActorForwardVector(), (ForwardMovementBaseSpeed + GetActorTransform().GetLocation().X / 10000.0f));
+}
---
+++ Source/PaintersSummit/Public/BoardController.h
@@ -0,0 +1,39 @@
+// Author: Matan Botansky
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Components/InputComponent.h"
+#include "BoardController.generated.h"
+
+UCLASS()
+class PAINTERSSUMMIT_API ABoardController : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABoardController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) 
+		float ForwardMovementBaseSpeed = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) 
+		float HorizontalMovementBaseSpeed = 1.0f;
+
+private:
+	void MoveHorizontal(float HorizontalMovement);
+	void MoveForward();
+
+};
